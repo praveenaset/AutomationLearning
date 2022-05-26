e1aa5881587612fd723b6b4ea495f16e9c38c032
prave <prave@192.168.1.33>
2022-05-26 13:15:56 +0530

    commit3

--- ForTest/src/com/utiba/qa/test/HelloWorld.java
+++ ForTest/src/com/utiba/qa/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("hey");
-		System.out.println("hey2");
+		System.out.println("hey245");
 	}
 
 }
